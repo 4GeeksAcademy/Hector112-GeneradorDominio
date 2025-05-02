6758899c0a2761d92b71818141bdfa62395e52ef
Héctor Fernández <60543953+Porki5@users.noreply.github.com>
2025-05-02 10:32:06 +0000

    lista urls

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+let pronombres = ['the', 'our'];
+let adjs = ['great', 'big'];
+let nombres = ['jogger', 'racoon'];
+let ext = ['.com', '.es'];
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const lista = document.getElementById("url")
+lista.innerHTML = ""
+for (let inicio of pronombres){
+   for (let medio of adjs){
+     for (let fin of nombres){
+       for (let puntos of ext){
+        let dominios = "www." + inicio + medio + fin + puntos   
+            console.log (dominios)
+            const items = document.createElement ("li")
+            items.textContent = dominios
+            lista.appendChild (items)
+        };
+      };
+    };
+  };
+  
